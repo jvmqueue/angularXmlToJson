48423e5732797fc19c4caef037ba60bfaa520c73
jvmqueue <wheels0090@yahoo.com>
2016-05-02 19:45:15 -0400

    begin using recurrsion to tranform XML to JSON

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,9 @@ jvm.mainApp.controller('controllerMain', ['$scope', '$http', 'util', function($s
 
     var self = this;
     $scope.buttons = jvm.buttons;
+    self.hash = {};
+    self.documentElement;
+    self.intCounter = 0;
 
     var _func = {
         request:function(paramPath){
@@ -23,12 +26,7 @@ jvm.mainApp.controller('controllerMain', ['$scope', '$http', 'util', function($s
 
                 var data = response.data;
                 var xmlResponseXml = $(response.data)[2];
-
                 var jsonFromXml = _func.convertXmlToJson(xmlResponseXml);
-
-                console.group('SUCCESS CALL BACK');
-                    console.log('jsonFromXml:\t', jsonFromXml);
-                   console.groupEnd(); 
 
             }, function errorCallback(){
                 console.group('ERROR CALL BACK');
@@ -36,66 +34,73 @@ jvm.mainApp.controller('controllerMain', ['$scope', '$http', 'util', function($s
                    console.groupEnd(); 
             })
         }, // End request
-        convertXmlToJson:function(paramXml){
-
+        convertXmlToJson:function(paramXml, paramObj, paramHashName){
             var xml = paramXml;
-            var obj = {};
-            var nodeAttribute = null;
-            var blnNotPrintable = true; // assume not a printable string
+            var nodeElement = null;
+            var nodeName = null;
+            var strHashName = paramHashName || null;
+            var text = null;
+            var obj = paramObj || {};
 
-            if(xml.nodeType == 1){
-                var len = xml.attributes.length;
-                if(len > 0){
-                    obj['@attributes'] = {};
-                    for(var i = 0; i < len; i++){
-                        nodeAttribute = xml.attributes.item(i);
-                        obj['@attributes'][nodeAttribute.nodeName] = nodeAttribute.nodeValue;
-                    }
-                } // End inner if
-            }else if(xml.nodeType == 3){
-                var text = xml.nodeValue;
-                blnNotPrintable = util.regex.blnNotPrintable(text); // service
 
-                if( (blnNotPrintable === false) && text.length > 1 ){ 
-                    obj = text;                     
-                }
-                
-            }
+            var blnIsNotPrintable = false;
+            
+            var ELEMENT_NODE = 1;
+            var TEXT_NODE = 3;
 
-            if(xml.hasChildNodes){
-                for(var i =0, len = xml.childNodes.length; i < len; i++){
-                    var item = xml.childNodes.item(i);
-                    var nodeName = item.nodeName;
+            if(xml.nodeType === ELEMENT_NODE){
+                   if(xml.firstChild.nodeValue){
+                        strHashName = xml.nodeName;
+                        text = xml.firstChild.nodeValue;
+                        if(strHashName in obj){
+                            var objTemp = {};
+                            objTemp[strHashName] = obj[strHashName];                            
 
-                    nodeName = util.regex.strReplace(nodeName, '#', ''); // replace #text with text
-                    
-                    if( typeof( obj[nodeName] == 'undefined' ) ){
-                        blnNotPrintable = util.regex.blnNotPrintable(item.data); // service
-                        if(blnNotPrintable === true){
-                            continue; // avoid storing objects of #text
-                        }
-                        // TODO: if collection of children like option, option, option, store in an array object
-
-                        obj[nodeName] = _func.convertXmlToJson(item); // recursion
-                    }else{
-                        if( typeof( obj[nodeName] ).push == 'undefined' ){
-                            var old = obj[nodeName];
-                            obj[nodeName] = [];
-                            obj[nodeName].push(old);
+                            if( Array.isArray(obj[strHashName]) === true){
+                                obj[strHashName].push(text);
+                                objTemp = null;
+                            }else{
+                                obj[strHashName] = [];
+                                obj[strHashName].push(objTemp[strHashName]);
+                                obj[strHashName].push(text);
+                            }
+                        }else{
+                            blnIsNotPrintable = util.regex.blnNotPrintable(text)
+                            blnIsNotPrintable === true ? obj[strHashName] = {} : obj[strHashName] = xml.firstChild.nodeValue;                        
                         }
 
-                        obj[nodeName].push( _func.convertXmlToJson(item) );
+                   }
+            }else if(xml.nodeType === TEXT_NODE){
+                console.group('NODE TYPE 3');
+                    console.log('xml:\t', xml);
+                   console.groupEnd(); 
+            } 
+
+            if(!self.documentElement){
+                self.documentElement = xml.nodeName;
+                self.hash[self.documentElement]; 
+            }            
+
+            if( xml && xml.hasChildNodes ){
+                var nodeChilds = xml.childNodes;
+                for(var i = 0, len = nodeChilds.length; i < len; i++){
+                    if(nodeChilds[i].nodeType ===  1){
+                        _func.convertXmlToJson(nodeChilds[i], obj, strHashName);
                     }
-                }
+                }                                
             }
-            return obj;
+
+            console.group('CONVERT XML TO JSON');
+                console.log('obj:\t', obj);
+               console.groupEnd(); 
+
         }  // End convertXmlToJson
     }; // End _func
 
     self.init = (function(){
 
         $http.defaults.headers.common.Accept = 'text/xml';
-        _func.request('/angular/site/data/index.xml');
+        _func.request('data/index.xml');
     })();
 
 
