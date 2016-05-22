a883d87cf0c342bbbcc430c07c23f6308ea9d4a9
jvmqueue <wheels0090@yahoo.com>
2016-05-22 18:56:27 -0400

    implementing recursion to parse XML to JSON. Close, but incomplete...

--- scripts/xmlToJson.js
+++ scripts/xmlToJson.js
@@ -8,7 +8,46 @@ jvm.xmlToJson = (function(){
 
     var ELEMENT_NODE = 1;
     var TEXT_NODE = 3;    
+    var mIntCounter = 0;  
+    var mStrTempDelete = '';
     
+    var ObjXml = function(){
+        if(this.blnIsInstantiated !== true){ // enforce singleton, can be instantiated once
+            return this;
+        }
+        this.blnIsInstantiated = true;
+    };
+    ObjXml.prototype = {
+        getTagName:function(paramNode){
+            return paramNode.nodeName;
+        },
+        getNextElement:function(paramNode){
+            var nodeParent = paramNode;
+            var space = ' ';
+            var json = {};
+
+            for(var i = 0, len = paramNode.childNodes.length; i < len; i++){
+                if(paramNode.childNodes[i].nodeType === ELEMENT_NODE){
+                    mStrTempDelete += paramNode.childNodes[i].nodeName  + ', ';
+                    json[paramNode.childNodes[i].nodeName] = paramNode.childNodes[i].firstChild.nodeValue;
+                    console.group('paramNode.childNodes[i]');
+                        console.log('paramNode.childNodes[i].nodeName:\t', paramNode.childNodes[i].nodeName);
+//                        console.log('strTempDelete:\t', mStrTempDelete);
+                        console.log('json:\t', json);
+                       console.groupEnd(); 
+                }
+            }
+            
+            for(var name in nodeParent){
+                if(name === 'children'){ // go deep into children nodes
+                    for(var i = 0, len = nodeParent[name].length; i < len; i++){
+                        this.getNextElement(nodeParent[name][i]);
+                    }
+                }
+            }
+        }
+    };
+
     var _fnc = {
         getAttributes:function(paramNode){
             var node = paramNode;
@@ -19,81 +58,26 @@ jvm.xmlToJson = (function(){
             }
             return jsonAttributes;
         },
-        convertXmlToJson:function(options){
+        convertXmlToJson:function(options, fncXml){
             var xml = options.xml;
-            var nodeElement = null;
+            var returnObjJson = {};
+            var obj = {};
             var nodeName = null;
-            var strHashName = options.hashName || null;
-            var text = null;
-            var obj = options.obj || {};
-            var objJson = null;
-            var attributes = null;
-            
-            var blnIsNotPrintable = false;
-            strHashName != null ? strHashName = strHashName.toLowerCase():'';
-            if(strHashName in obj){
-                obj = obj[strHashName];
+
+            if(!!xml.documentElement){
+                nodeName = fncXml.getTagName(xml.documentElement);
+                obj[nodeName] = {}; // {"resume":{}}
+                fncXml.getNextElement(xml.documentElement);
             }
 
 
-            if(xml.nodeType === ELEMENT_NODE){
-                   if(xml.firstChild.nodeValue){
-                        strHashName = xml.nodeName.toLowerCase();
-                        text = xml.firstChild.nodeValue;
-                        attributes =  _fnc.getAttributes(xml);
-                        if(strHashName in obj){
-                            var objTemp = {};
-                            objTemp[strHashName] = obj[strHashName];                            
 
-                            if( Array.isArray(obj[strHashName]) === true){
-                                obj[strHashName].push({text:text, attributes:attributes});
-                                objTemp = null;
-                            }else{
-                                blnIsNotPrintable = jvm.regEx.fnc.blnNotPrintable(text);    
-                                if(!blnIsNotPrintable){ 
-                                    obj[strHashName] = [];
-                                    obj[strHashName].push(objTemp[strHashName]);
-                                    obj[strHashName].push({text:text, attributes:attributes});                                    
-                                }
-                            }
-                        }else{
-                            blnIsNotPrintable = jvm.regEx.fnc.blnNotPrintable(text);
-                            attributes =  _fnc.getAttributes(xml);    
-                            if(blnIsNotPrintable === true){
-                                obj[strHashName] = {}
-                            }else{
-                                obj[strHashName] = {text:xml.firstChild.nodeValue, attributes:attributes};
-                            }
-                                                    
-                        }
-
-                   }
-            }else if(xml.nodeType === TEXT_NODE){
-                console.group('NODE TYPE 3');
-                    console.log('xml:\t', xml);
-                   console.groupEnd(); 
-            } 
-
-            if(!self.documentElement){
-                self.documentElement = xml.nodeName;
-                self.hash[self.documentElement] = {}; 
-            }            
-
-            if( xml && xml.hasChildNodes ){
-                var nodeChilds = xml.childNodes;
-                for(var i = 0, len = nodeChilds.length; i < len; i++){
-                    if(nodeChilds[i].nodeType ===  ELEMENT_NODE){
-                        _fnc.convertXmlToJson({xml:nodeChilds[i], hashName:strHashName, obj:obj});
-                    }
-                }                                
-            }
-
-            objJson = obj;
-            return objJson;
+            returnObjJson = obj;
+            return returnObjJson;
 
         }  // End convertXmlToJson
 
     }; // End _fnc
 
-    return{fnc:_fnc}
+    return{fnc:_fnc, ObjXml:ObjXml}
 })();
